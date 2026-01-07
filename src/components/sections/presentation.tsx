9cf5b2d443461309e333b6e70a534c70adc9e5b7
keven <kevengoulartmm@gmail.com>
2026-01-07 09:43:38 -0300

    add resume (curriculum) to presentation section

--- src/components/sections/presentation.tsx
+++ src/components/sections/presentation.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { FaEnvelope, FaGithub, FaLinkedin } from 'react-icons/fa'
+import { FaEnvelope, FaFilePdf, FaGithub, FaLinkedin } from 'react-icons/fa'
 import { useTranslations } from 'next-intl'
 import BlobComponentShinji from '../Blob-shinji'
 
@@ -15,7 +15,7 @@ export default function Presentation() {
         <h2 className="text-3xl tracking-wide font-semibold text-slate-900/85 max-sm:text-center">
           {t('description')}
         </h2>
-        <div className="flex items-center justify-center gap-8 mt-6">
+        <div className="flex items-center justify-center gap-6 mt-6">
           <div className="flex items-center justify-center border-2 border-slate-900 rounded-full p-2 group cursor-pointer hover:border-slate-900/50 hover:scale-125 transition-transform duration-300">
             <Link
               href="https://www.linkedin.com/in/k%C3%A9ven-goulart-890248215/"
@@ -34,6 +34,12 @@ export default function Presentation() {
           <div className="flex items-center justify-center border-2 border-slate-900 rounded-full p-2 group cursor-pointer hover:border-slate-900/50 hover:scale-125 transition-transform duration-300">
             <Link href="mailto:kevengoulartmm@gmail.com" target="_blank">
               <FaEnvelope className="size-12 group-hover:text-slate-900/70" />
+            </Link>
+          </div>
+
+          <div className="flex items-center justify-center border-2 border-slate-900 rounded-full p-2 group cursor-pointer hover:border-slate-900/50 hover:scale-125 transition-transform duration-300">
+            <Link href={t('link')} target="_blank">
+              <FaFilePdf className="size-12 group-hover:text-slate-900/70" />
             </Link>
           </div>
         </div>
